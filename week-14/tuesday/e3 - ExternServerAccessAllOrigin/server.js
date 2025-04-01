95cc61ee698504e7fdbf12ce18d0b20cdf18130f
Neo <neogoldahlgren@gmail.com>
2025-04-01 10:14:11 +0200

    trolle in lesson haha

--- week-14/tuesday/e3 - ExternServerAccessAllOrigin/server.js
+++ week-14/tuesday/e3 - ExternServerAccessAllOrigin/server.js
@@ -1,32 +1,27 @@
 function handler(req) {
-    const CORS = new Headers();
-    CORS.set("Access-Control-Allow-Origin", "*");
-    CORS.set("Content-Type", "application/json");
-    console.log(CORS);
+    const headers = new Headers();
+    headers.set("Access-Control-Allow-Origin", "*");
+    // headers.set("Content-Type", "application/json");
     if (req.method === "OPTIONS") {
-        return new Response(null, {headers: CORS});
+        return new Response(null, {headers: headers});
     }
 
     if (req.method === "GET" && req.url === "http://localhost:8000/randomColor") {
         let colors = ["pink", "lime", "skyblue", "lightgreen", "gold"];
-        // let chosenColor = colors[Math.floor(Math.random() * colors.length)];
-        let a = [];
-        let uniques = false;
-        while (!uniques) {
-            let colorOne = colors[Math.floor(Math.random() * colors.length)];
-            let colorTwo = colors[Math.floor(Math.random() * colors.length)];
+        return new Response(colors[Math.floor(Math.random() * colors.length)], {headers: headers});
+        // let a = [];
+        // let uniques = false;
+        // while (!uniques) {
+        //     let colorOne = colors[Math.floor(Math.random() * colors.length)];
+        //     let colorTwo = colors[Math.floor(Math.random() * colors.length)];
 
-            if (colorOne === colorTwo) {
-                continue;
-            } else {
-                a.push(colorOne, colorTwo);
-                uniques = true;
-            }
-        }
-        // return new Response(chosenColor, {headers: CORS});
-        return new Response(JSON.stringify(a), {headers: CORS});
+        //     if (colorOne !== colorTwo) {
+        //         uniques = true;
+        //     } 
+        // }
+        // return new Response(JSON.stringify(a), {headers: CORS});
     } else {
-        return new Response("Invalid Request", {status: 400, headers: CORS});
+        return new Response("Invalid Request", {status: 400, headers: headers});
     }
 }
 
